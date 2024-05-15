250522bc9e0719d7a45fc2c2fc55618931460bd4
Knight-ZXW <nimdanoob@163.com>
2024-05-16 00:10:54 +0800

    fix resume therad

--- kbArt/src/main/cpp/art/thread_list.cpp
+++ kbArt/src/main/cpp/art/thread_list.cpp
@@ -10,13 +10,14 @@ namespace art {
 void *ThreadList::SuspendThreadByThreadId(uint32_t threadId,
                                           SuspendReason suspendReason,
                                           bool *timed_out) {
-  int32_t api_level = getAndroidApiLevel();
+
   static void *(*suspend_thread_by_thread_id)(void *,
                                               uint32_t,
                                               uint8_t,
                                               bool *) = nullptr;
   if (suspend_thread_by_thread_id == nullptr) {
     std::string symbol;
+    int32_t api_level = getAndroidApiLevel();
     if (api_level > 26 && api_level <= 34) {
       symbol = "_ZN3art10ThreadList23SuspendThreadByThreadIdEjNS_13SuspendReasonEPb";
     } else {
@@ -30,9 +31,9 @@ void *ThreadList::SuspendThreadByThreadId(uint32_t threadId,
                                    bool *)>(findArtSoSym(
             symbol.c_str()));
     if (suspend_thread_by_thread_id == nullptr) {
-      LOGE("zxw", "suspend_thread_by_thread_id func is null");
+      LOGE("kbArt", "suspend_thread_by_thread_id func is null");
     } else {
-      LOGE("zxw",
+      LOGE("kbArt",
            "函数不为空 suspend_thread_by_thread_id func is %p",
            suspend_thread_by_thread_id);
     }
@@ -46,10 +47,18 @@ bool ThreadList::Resume(void *thread, SuspendReason suspendReason) {
   //TODO 8.1以下版本做区分，其函数签名不一致
   static bool (*resume)(void *, void *, SuspendReason) = nullptr;
   if (resume == nullptr) {
+    std::string symbol;
+    int32_t api_level = getAndroidApiLevel();
+    if (api_level > 26 && api_level <= 34) {
+      symbol = "_ZN3art10ThreadList6ResumeEPNS_6ThreadENS_13SuspendReasonE";
+    } else {
+      symbol = "_ZN3art10ThreadList6ResumeEPNS_6ThreadEb";
+    }
     resume = reinterpret_cast<bool (*)(void *thread_list,
                                        void *thread,
                                        SuspendReason suspendReason)>(findArtSoSym(
-        "_ZN3art10ThreadList6ResumeEPNS_6ThreadENS_13SuspendReasonE"));
+        symbol.c_str()));
+    LOGD("kbArt","resume thread %p",thread);
   }
   return resume(this, thread, suspendReason);
 }
